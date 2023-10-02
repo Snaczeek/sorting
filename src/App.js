d3c1c05339e2ad20300aff863066951600ef9623
Snaczeek <mateusz0filoda@gmail.com>
2023-10-02 20:10:01 +0200

    small changes
    added new title for website
    deleted old readme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+import React, { useEffect } from 'react';
+
 
 function App() {
+  useEffect(() => {
+    document.title = 'Sorting Visualizer';
+  }, []);
+
   return (
+    
     <div className="App">
       <SortingVisualizer></SortingVisualizer>
       <footer>
